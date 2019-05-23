601a3209c83c9d584fce95fa652aeafe7687dd86
odiapratama <odiapratama@gmail.com>
2019-05-23 11:48:56 +0700

    fix

---
+++ build/precache-manifest.ee939bdbd04347a2a261ea3dc65e3dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ce1b9252f6a4f99d9895aea806e3f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d119d99e62c4fc68021",
+    "url": "/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "3d119d99e62c4fc68021",
+    "url": "/static/js/2.568e85e5.chunk.js"
+  },
+  {
+    "revision": "b8309f34f366d7898b97",
+    "url": "/static/js/main.b9fccb90.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
